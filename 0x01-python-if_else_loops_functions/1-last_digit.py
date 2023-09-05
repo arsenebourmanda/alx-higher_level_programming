dd81c4da9604d582e65a1a9350bceaa05d5fcb89
koumnomani bourmanda <arsenebourmanda@gmail.com>
2023-09-05 16:45:28 -0700

    adds content to  0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 numLD = abs(number) % 10
-if (number <0):
+if (number < 0):
     numLD = numLD * (-1)
 print(f"Last digit of {number} is {numLD} and is", end=" ")
 if (numLD > 5):
