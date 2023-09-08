b951e4d8a9d25781f04659ef79235c07fbfb661d
koumnomani bourmanda <arsenebourmanda@gmail.com>
2023-09-07 18:46:08 -0700

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,9 @@ if __name__ == "__main__":
     import sys
     if (len(sys.argv)-1) == 0:
         print("{} arguments.".format(len(sys.argv)-1))
+    elif (len(sys.argv)-1) == 1:
+        print("{} argument:".format(len(sys.argv)-1))
+        print("{}: {}".format(1, sys.argv[1]))
     else:
         print("{} arguments:".format(len(sys.argv)-1))
         for a in range(len(sys.argv)-1):
