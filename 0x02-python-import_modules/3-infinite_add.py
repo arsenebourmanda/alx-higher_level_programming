ed5fced0e1a48cc9c24ab5a56864b6ffdca06069
koumnomani bourmanda <arsenebourmanda@gmail.com>
2023-09-07 18:01:41 -0700

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     mySum = 0
-    for a in range(len(sys.argv) - 1):
-        mySum = mySum + int(sys.argv[a + 1])
+    for a in range(len(sys.argv)-1):
+        mySum = mySum + int(sys.argv[a+1])
     print(mySum)
 
