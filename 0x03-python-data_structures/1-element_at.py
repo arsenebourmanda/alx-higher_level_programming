7b6a17c11eeaa20bc97811c7d6294911c7b64ba0
koumnomani bourmanda <arsenebourmanda@gmail.com>
2023-09-10 06:21:44 -0700

    update

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    if (idx < 0 and idx >(len(my_list)-1)):
-        return
-    else:
-        print("{}".format(my_list[idx]))
+if __name__ == "__main__":
+    def element_at(my_list, idx):
+        if (idx < 0 and idx >(len(my_list)-1)):
+            return
+        else:
+            print("{}".format(my_list[idx]))
